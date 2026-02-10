f1f0ee8c9c0b946e7da21a9cd72ba9897bba077b
JordanBushey <jordanbushey83@gmail.com>
2026-02-10 13:31:50 -0500

    Working on ATM to practice using contructors

--- atm.cpp
+++ atm.cpp
@@ -8,16 +8,44 @@ class mainMenu
 public:
     void displayMenu()
     {
-        cout << "Heres the menu" << endl;
+        if ( menuState == 0 )
+            cout << "Welcome to the ATM" << endl;
+            cout << "Please enter an account number: " << endl;
+
+        if ( menuState == 1 )
+            cout << "1. View my balance \n2. Withdraw Cash\n3. Deposit Cash\n4. Exit" << endl;
     }
+
+    void setMenuState( int newState )
+    {
+        menuState = newState;
+    }
+private:
+    int menuState;
 };
 
 class Account
 {
 public:
+    Account( int newAccountNumber, int newAccountBalance )
+    {
+        setAccountBalance(newAccountBalance);
+        setAccountNumber(newAccountNumber);
+    }
+
     int getAccountBalance()
     {
         return accountBalance;
+    }
+
+    void setAccountBalance( int newAccountBalance )
+    {
+        accountBalance = newAccountBalance;
+    }
+
+    void setAccountNumber( int newAccountNumber )
+    {
+        accountNumber = newAccountNumber;
     }
 
     void deposit( int value )
@@ -31,13 +59,16 @@ public:
     }
 private:
     int accountBalance;
+    int accountNumber;
 };
 
 
 int main()
 {
-    cout << "Welcome to my atm!" << endl;
     mainMenu myMainMenu;
+    Account myAccount(1, 0);
+    myMainMenu.displayMenu();
+    myMainMenu.setMenuState(1);
     myMainMenu.displayMenu();
     return 0;
 }

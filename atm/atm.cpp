0c2017caf4ded93def1139088721c3d8b48e6598
JordanBushey <jordanbushey83@gmail.com>
2026-02-10 20:05:43 -0500

    Deposit, withdraw and view account balance now working, had to pass by refrence

--- atm/atm.cpp
+++ atm/atm.cpp
@@ -12,21 +12,37 @@ public:
     {
         menuState = newMenuState;
     }
-    void displayMenu()
+    void displayMenu( Account& userAccount )
     {
         if ( menuState == 0 )
         {
             cout << "Welcome to the ATM" << endl;
             cout << "Please enter an account number: " << endl;
+            // cin >> accountNumber;
         }
         else if ( menuState == 5 )
             cout << "1. View my balance \n2. Withdraw Cash\n3. Deposit Cash\n4. Exit" << endl;
+
         else if ( menuState == 1 )
-            cout << "Account Balance: " << endl;
+            cout << "Account Balance: " << userAccount.getAccountBalance() << endl;
+
         else if ( menuState == 2 )
-            cout << "Withdraw Cash\n Please enter an amount to withdraw: " << endl;
+        {
+            cout << "Withdraw Cash\nPlease enter an amount to withdraw: " << endl;
+            cin >> x;
+            cout << "\nWithdrawing " << x << endl;
+            if( userAccount.getAccountBalance() < x)
+                cout << "You do not have enough in your account to make this withdrawl" << endl;
+            else
+                userAccount.withdraw(x);
+        }
         else if ( menuState == 3 )
-            cout << "Deposit Cash\n Pleaste enter an amount to deposit: " << endl;
+        {
+            cout << "Deposit Cash\nPlease enter an amount to deposit: " << endl;
+            cin >> x;
+            cout << "Depositing " << x << endl;
+            userAccount.deposit(x);
+        }
         else if ( menuState == 4 )
             cout << "Exit" << endl;
     }
@@ -37,20 +53,27 @@ public:
     }
 private:
     int menuState;
+    int x;
 };
 
 
 
 int main()
 {
-    Menu myMenu(1);
-    Account myAccount(1, 0);
-    myMenu.displayMenu();
+    bool running = true;
     int x;
-    cout << "\nSelect a menu option: ";
-    cin >> x;
 
-    myMenu.setMenuState(x);
-    myMenu.displayMenu();
+    Menu myMenu(5);
+    Account myAccount(1, 0);
+
+    while(running == true)
+    {
+        myMenu.displayMenu( myAccount );
+        cout << "\nSelect a menu option: ";
+        cin >> x;
+        if(x == 4)
+            break;
+        myMenu.setMenuState(x);
+    }
     return 0;
 }
